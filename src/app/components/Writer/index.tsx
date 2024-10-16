9b426621f4a0c9950b51ff19de4c3f3f8588bbed
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-10-15 21:47:07 -0400

    refactor: update the Writer component to handle creating new posts

--- src/app/components/Writer/index.tsx
+++ src/app/components/Writer/index.tsx
@@ -5,18 +5,11 @@ import Highlight from "@tiptap/extension-highlight";
 import Typography from "@tiptap/extension-typography";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { MY_SITE_URL } from "@/app/utils/constants";
+import { useRouter } from "next/navigation";
 
-type Props = {
-  handleOnPostSave: (
-    postStatus: String,
-    postTitle: String,
-    postContent: String,
-    postCategory: String,
-  ) => void;
-};
-
-const Writer = ({ handleOnPostSave }: Props) => {
-  const [postStatus, setPostStatus] = useState("");
+const Writer = () => {
+  const { push } = useRouter();
   const [postTitle, setPostTitle] = useState("");
   const [postContent, setPostContent] = useState("");
   const [postCategory, setPostCategory] = useState("");
@@ -29,28 +22,49 @@ const Writer = ({ handleOnPostSave }: Props) => {
     immediatelyRender: false, // Need to pass this attribute to prevent rehydration mismatch when using SSR
   });
 
+  const handleOnPostSave = async (
+    postTitle: String,
+    postContent: String,
+    postCategory: String,
+  ) => {
+    try {
+      await fetch(`${MY_SITE_URL}/api/post`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: postTitle,
+          content: postContent,
+          status: "DRAFT",
+          category: postCategory,
+          // TODO: Need to use the current userId as reference instead of a hardcoded value
+          userId: "41a75ede-1cbd-4002-8475-f7aef583cc7e",
+        }),
+      });
+      push("/admin");
+    } catch (e) {
+      console.error("Failed to save the post in the database");
+    }
+  };
+
   return (
     <div>
-      <button
-        onClick={() =>
-          handleOnPostSave(postStatus, postTitle, postContent, postCategory)
-        }
-      >
-        Save
-      </button>
-      <input
-        value={postStatus}
-        onChange={(event) => setPostStatus(event.target.value)}
-      />
       <input
         value={postTitle}
         onChange={(event) => setPostTitle(event.target.value)}
       />
-      <EditorContent editor={editor} />
       <input
         value={postCategory}
         onChange={(event) => setPostCategory(event.target.value)}
       />
+      <EditorContent editor={editor} />
+      <button
+        onClick={() => handleOnPostSave(postTitle, postContent, postCategory)}
+      >
+        Save
+      </button>
     </div>
   );
 };
