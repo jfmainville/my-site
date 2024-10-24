c6184fad4d7449b83de33d8548dc582b2471eda5
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-10-23 21:08:35 -0400

    refactor: update the styling of the AdminPage page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,28 +1,45 @@
 import styles from "./page.module.scss";
 import Link from "next/link";
 import Navbar from "../components/Navbar";
+import { Post } from "@prisma/client";
 import { MY_SITE_URL } from "../utils/constants";
+import Button from "../components/Button/index";
 
 const AdminPage = async () => {
-  let data = await fetch(`${MY_SITE_URL}/api/post`);
-  let posts = await data.json();
+  let posts;
+  let response = await fetch(`${MY_SITE_URL}/api/post`, { cache: "no-store" });
+
+  if (response.ok) {
+    posts = await response.json();
+  }
 
   return (
     <section className={styles.Main}>
       <Navbar />
-      <section>Blog Posts</section>
+      <section>
+        <h2>Blog Posts</h2>
+      </section>
       <Link className={styles.Link} href={"/admin/post"}>
-        Create Post
+        <Button buttonText={"Create Post"} />
       </Link>
-      {posts.data.map((post: any) => (
-        <Link
-          className={styles.Link}
-          key={post.id}
-          href={`/admin/post/${post.slug}`}
-        >
-          {post.title}
-        </Link>
-      ))}
+      {posts.data ? (
+        posts.data?.map((post: Post) => (
+          <div key={post.id} className={styles.BlogPost}>
+            <Link className={styles.Link} href={`/admin/post/${post.slug}`}>
+              {post.title}
+            </Link>
+            <div className={styles.BlogPostStatus}>{post.status}</div>
+            <div className={styles.BlogPostCreatedDate}>
+              {post.createdAt.toString()}
+            </div>
+            <div className={styles.BlogPostUpdatedDate}>
+              {post.updatedAt.toString()}
+            </div>
+          </div>
+        ))
+      ) : (
+        <div>No posts</div>
+      )}
     </section>
   );
 };
