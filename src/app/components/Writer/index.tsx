26c957f9aeb947b68c8b57e9c91bb7422d89c7ae
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-12-30 14:43:05 -0500

    feat: add the styling configuration for the TipTap editor component

--- src/app/components/Writer/index.tsx
+++ src/app/components/Writer/index.tsx
@@ -1,18 +1,35 @@
 "use client";
 
 import styles from "./index.module.scss";
-import { useState } from "react";
-import Highlight from "@tiptap/extension-highlight";
-import Typography from "@tiptap/extension-typography";
+import { useState, useCallback } from "react";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import CodeBlock from "@tiptap/extension-code-block";
+import Dropcursor from "@tiptap/extension-dropcursor";
+import Image from "@tiptap/extension-image";
 import { MY_SITE_URL } from "@/app/utils/constants";
 import { useRouter } from "next/navigation";
 import Button from "../../components/Button/index";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBold,
+  faItalic,
+  faList,
+  faListOl,
+  faStrikethrough,
+  faCode,
+  faImage,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Writer = ({ postData }: any) => {
   const router = useRouter();
   const [postTitle, setPostTitle] = useState(postData ? postData.title : "");
+  const [postThumbnail, setPostThumbnail] = useState(
+    postData ? postData.thumbnail : "",
+  );
+  const [postStatus, setPostStatus] = useState(
+    postData ? postData.status : "DRAFT",
+  );
   const [postContent, setPostContent] = useState(
     postData ? postData.content : "Enter text here...",
   );
@@ -20,7 +37,12 @@ const Writer = ({ postData }: any) => {
     postData ? postData.category : "",
   );
   const editor = useEditor({
-    extensions: [StarterKit, Highlight, Typography],
+    editorProps: {
+      attributes: {
+        class: styles.WriterEditor,
+      },
+    },
+    extensions: [StarterKit, CodeBlock, Image, Dropcursor],
     content: postContent,
     onBlur({ editor }) {
       setPostContent(editor.getHTML());
@@ -30,6 +52,7 @@ const Writer = ({ postData }: any) => {
 
   const handleOnPostSave = async (
     postTitle: String,
+    postThumbnail: String,
     postContent: String,
     postCategory: String,
   ) => {
@@ -44,12 +67,13 @@ const Writer = ({ postData }: any) => {
             },
             body: JSON.stringify({
               title: postTitle,
+              thumbnail: postThumbnail,
               slug: postTitle.replace(" ", "-").toLowerCase(),
               content: postContent,
               status: "DRAFT",
               category: postCategory,
               // TODO: Need to use the current userId as reference instead of a hardcoded value
-              userId: "874128cf-ae33-4836-89d1-4312a3e4d481",
+              userId: "seed-user-1",
             }),
           });
         } catch (e) {
@@ -66,12 +90,13 @@ const Writer = ({ postData }: any) => {
             body: JSON.stringify({
               id: postData.id,
               title: postTitle,
+              thumbnail: postThumbnail,
               slug: postTitle.replace(" ", "-").toLowerCase(),
               content: postContent,
               status: postData.status,
               category: postCategory,
               // TODO: Need to use the current userId as reference instead of a hardcoded value
-              userId: "874128cf-ae33-4836-89d1-4312a3e4d481",
+              userId: "seed-user-2",
             }),
           });
         } catch (e) {
@@ -114,6 +139,17 @@ const Writer = ({ postData }: any) => {
     }
   };
 
+  const addImage = useCallback(() => {
+    const url = window.prompt("URL");
+
+    if (url) {
+      editor.chain().focus().setImage({ src: url }).run();
+    }
+  }, [editor]);
+  if (!editor) {
+    return null;
+  }
+
   return (
     <div className={styles.Writer}>
       <input
@@ -136,6 +172,118 @@ const Writer = ({ postData }: any) => {
         placeholder="Thumbnail"
         onChange={(event) => setPostThumbnail(event.target.value)}
       />
+      <div className={styles.WriterEditorBar}>
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={
+            editor.isActive("bold")
+              ? styles.WriterEditorButtonActive
+              : styles.WriterEditorButton
+          }
+        >
+          <FontAwesomeIcon icon={faBold} width="40" height="40" />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={
+            editor.isActive("italic")
+              ? styles.WriterEditorButtonActive
+              : styles.WriterEditorButton
+          }
+        >
+          <FontAwesomeIcon icon={faItalic} width="40" height="40" />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          className={
+            editor.isActive("strike")
+              ? styles.WriterEditorButtonActive
+              : styles.WriterEditorButton
+          }
+        >
+          <FontAwesomeIcon icon={faStrikethrough} width="40" height="40" />
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 1 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 1 })
+              ? styles.WriterEditorButtonActive
+              : styles.WriterEditorButton
+          }
+          style={{ width: "2.5rem" }}
+        >
+          H1
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 2 })
+              ? styles.WriterEditorButtonActive
+              : styles.WriterEditorButton
+          }
+          style={{ width: "2.5rem" }}
+        >
+          H2
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 3 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 3 })
+              ? styles.WriterEditorButtonActive
+              : styles.WriterEditorButton
+          }
+          style={{ width: "2.5rem" }}
+        >
+          H3
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={
+            editor.isActive("bulletList")
+              ? styles.WriterEditorButtonActive
+              : styles.WriterEditorButton
+          }
+        >
+          <FontAwesomeIcon icon={faList} width="40" height="40" />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={
+            editor.isActive("orderedList")
+              ? styles.WriterEditorButtonActive
+              : styles.WriterEditorButton
+          }
+        >
+          <FontAwesomeIcon icon={faListOl} width="40" height="40" />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+          className={
+            editor.isActive("codeBlock")
+              ? styles.WriterEditorButtonActive
+              : styles.WriterEditorButton
+          }
+        >
+          <FontAwesomeIcon icon={faCode} width="40" height="40" />
+        </button>
+        <button
+          onClick={addImage}
+          className={
+            editor.isActive("image")
+              ? styles.WriterEditorButtonActive
+              : styles.WriterEditorButton
+          }
+        >
+          <FontAwesomeIcon icon={faImage} width="40" height="40" />
+        </button>
+      </div>
+      <EditorContent data-testid="TipTap" editor={editor} />
       <div className={styles.WriterButtons}>
         <Button
           onButtonClickEvent={() =>
