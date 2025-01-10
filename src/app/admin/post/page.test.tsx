174f15f32fc38cd04ce53589eef2073331f41a11
Jean-Frederic Mainville <jfmainville@outlook.com>
2025-01-09 22:24:43 -0500

    refactor: update the tests configuration in the PostPage page

--- src/app/admin/post/page.test.tsx
+++ src/app/admin/post/page.test.tsx
@@ -1,52 +1,37 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import PostPage from "./page";
-import { mockFetch } from "../../utils/mockFetch";
+import { getPosts } from "@/app/lib/db";
 
-jest.mock("next/navigation", () => ({
-  useRouter() {
-    return {
-      prefetch: () => null,
-      push: jest.fn(),
-      refresh: jest.fn(),
-    };
-  },
-}));
+jest.mock("../../lib/db");
 
 describe("<PostPage/> Test Suite", () => {
   it("should render the <PostPage/> page unchanged", async () => {
-    window.fetch = mockFetch({});
     const { container } = render(await PostPage());
     expect(container).toMatchSnapshot();
   });
 
-  it("should return existing posts in the <AdminPage/> page", async () => {
-    window.fetch = mockFetch({
-      data: [
-        {
-          id: "0eb6cb4f-2020-4465-bb89-c4081753c71e",
-          slug: "test-blog-post-1",
-          title: "Test Blog Post 1",
-          content: "<h1>Test Blog Post 1</h1>",
-          status: "DRAFT",
-          category: "Cybersecurity",
-          userId: "48eceb63-fc36-426f-85a5-58ccc850eade",
-          createdAt: "2024-10-16T23:45:01.000Z",
-          updatedAt: "2024-10-17T00:46:10.156Z",
-        },
-        {
-          id: "37314b8f-6d97-4daa-affa-1717864a8e0e",
-          slug: "test-blog-post-2",
-          title: "Test Blog Post 2",
-          content: "<h1>Test Blog Post 2</h1>",
-          status: "DRAFT",
-          category: "Cybersecurity",
-          userId: "830ba444-6d36-401a-95cc-28646bfad2d5",
-          createdAt: "2024-10-16T23:45:01.000Z",
-          updatedAt: "2024-10-16T23:45:01.000Z",
-        },
-      ],
-    });
+  it("should return existing posts in the <PostPage/> page", async () => {
+    (getPosts as jest.Mock).mockResolvedValueOnce([
+      {
+        id: "0eb6cb4f-2020-4465-bb89-c4081753c71e",
+        slug: "test-blog-post-1",
+        title: "Test Blog Post 1",
+        content: "<h1>Test Blog Post 1</h1>",
+        status: "DRAFT",
+        category: "Cybersecurity",
+        userId: "48eceb63-fc36-426f-85a5-58ccc850eade",
+      },
+      {
+        id: "37314b8f-6d97-4daa-affa-1717864a8e0e",
+        slug: "test-blog-post-2",
+        title: "Test Blog Post 2",
+        content: "<h1>Test Blog Post 2</h1>",
+        status: "DRAFT",
+        category: "Cybersecurity",
+        userId: "830ba444-6d36-401a-95cc-28646bfad2d5",
+      },
+    ]);
     render(await PostPage());
     const firstBlogPost = screen.getByText("Test Blog Post 1");
     const secondBlogPost = screen.getByText("Test Blog Post 2");
@@ -55,9 +40,6 @@ describe("<PostPage/> Test Suite", () => {
   });
 
   it("should return a placeholder if there are no posts in the <AdminPage/> page", async () => {
-    window.fetch = mockFetch({
-      data: null,
-    });
     render(await PostPage());
     const noPostMessage = screen.getByText("No posts");
     expect(noPostMessage).toBeInTheDocument();
