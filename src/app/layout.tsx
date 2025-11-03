c7e0b432ff1a1f4ae14faec0a3fdcec379edffad
Jean-Frederic Mainville <jfmainville@outlook.com>
2025-11-02 21:16:22 -0500

    refactor(auth): remove Auth0 integration and related dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <UserProvider>
-        <body className={inter.className}>{children}</body>
-      </UserProvider>
+      <body className={inter.className}>{children}</body>
       <GoogleAnalytics gaId="G-B14Q3RXS9N" />
     </html>
   );
